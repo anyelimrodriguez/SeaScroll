331b838904c57b84667e4ae488bb036792f1b456
Calvin <calvinwang1002@gmail.com>
2022-11-14 09:48:25 -0500

    Set up a connection to Cloud Postgres, and added Functionality to Home Page

--- sea_scroll/lib/main.dart
+++ sea_scroll/lib/main.dart
@@ -1,7 +1,32 @@
 import 'package:flutter/material.dart';
 import 'pages/intro.dart';
+import 'package:postgres/postgres.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  print('something');
+  var connection = PostgreSQLConnection("10.0.2.2", 5432, "postgres", username: "postgres", password: "V:EHT]3&R;BP5sQQ");
+  try {
+    await connection.open();
+  } catch (e) {
+    print(e);
+  }
+  print('happened');
+
+  List<List<dynamic>> results = await connection.query("SELECT * FROM users");
+  for (final row in results) {
+    var a = row[0];
+    var b = row[1];
+    var c = row[2];
+    var d = row[3];
+    if(d != null)
+      print("$a" + " " + b + " " + c + " " + d);
+    else
+      print("$a" + " " + b + " " + c);
+  }
+  print('finally');
+  connection.close();
+
   runApp(const MyApp());
 }
 
